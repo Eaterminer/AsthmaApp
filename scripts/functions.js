9ea5d004681629837940517df2c31deb1e8eb3de
Eaterminer <58890567+Eaterminer@users.noreply.github.com>
2021-03-19 13:16:12 +1300

    Delete scripts directory

--- scripts/functions.js
+++
@@ -1,3 +0,0 @@
-function copyright() {
-    alert("Logo: Freepik On Flaticon.com\nDialog Flow: Google\nyeButtona Images Credit Goes To: classroomclipart.com\nEverything Else: Peter Heeren (PeterHeerenEmail@gmail.com)");
-}
