86bb94bdccdab2f5b0a9274bbe5f42b36e40928d
Eaterminer <58890567+Eaterminer@users.noreply.github.com>
2021-04-15 11:34:29 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,13 +10,7 @@ self.addEventListener('install', e => {
         `/AsthmaApp/styles.css`,
         `/AsthmaApp/scripts.js`,
         `/AsthmaApp/settings.html`,
-        `/AsthmaApp/functions.js`,
-        `/AsthmaApp/review.html`,
-        `/AsthmaApp/tour.html`,
-        '/AsthmaApp/images/inhaler.png',
-        '/AsthmaApp/images/favicon.png',
-        '/AsthmaApp/reward/game/index.html',
-        '/AsthmaApp/reward/game/script.js'
+        `/AsthmaApp/functions.js`
       ])
         .then(() => self.skipWaiting());
     })
