683b0ddf003df8580b4fe28777f4a3497f7d40ed
Eaterminer <58890567+Eaterminer@users.noreply.github.com>
2021-04-15 11:12:56 +1200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,24 @@
+function inhalerPressed() {
+	w3.hide("#inhalerImg");
+	// OLD CODEvar playTimeWorth = 1800;//	var playTimeReady = localStorage.getItem("playTimeReady");//	var playTime = localStorage.getItem("playTime");////	if (playTimeReady == true) {//		playTime = playTime + playTimeWorth;//		console.log("playTimeReady == true");//	} else {//		playTimeReady = true;//		localStorage.setItem("playTimeReady", true);//		playTime = playTimeWorth;//		console.log(`playTimeReady == false, ${playTimeReady}`);//	}//	localStorage.setItem("playTime", playTime);
+	var playTime = localStorage.getItem("playTime");
+	if (localStorage.getItem("playTime") >= 0) {
+		playTime = +playTime + 1800;
+		console.log("true")
+	} else {
+		var playTime = 1800;
+		console.log("false")
+	}
+	localStorage.setItem("playTime", playTime)
+
+
+	w3.show("#randomImage");
+}
+
+function loadSettings() {
+	var settings = localStorage.getItem("settings")
+	if (settings == null) {
+		settings  = [1800]
+	}
+	document.getElementById("gamingTime").value = "1800";
+}
